eba5181a87b0a5d964da2c4dc50fafdbb0f9b92f
Roberto Cornice <roberto@punktquadrat.de>
2026-03-09 09:53:54 +0100

    Update Gradle wrapper checksum
    - Update `distributionSha256Sum` in `gradle-wrapper.properties` to match the Gradle 9.2.1 distribution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "de.robnice.philipstvcontrol"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
     }
 
